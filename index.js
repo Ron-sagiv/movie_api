c0884dd2ebe4fcf08453cd0bb053fb301dff21f2
Ron-sagiv <pedrozeeg@gmail.com>
2025-12-26 10:02:47 +0100

    refactor (js): endpoints now work in postman

--- index.js
+++ index.js
@@ -5,55 +5,77 @@ const express = require('express'),
 
 const app = express();
 app.use(bodyParser.json());
-//----------previous code----------------------
-// setup the logger
 app.use(morgan('common'));
 
-let myTopTenMovies = [
+let movies = [
   {
     title: 'The Dark Knight',
-    director: 'Christopher Nolan',
+    director: { name: 'Christopher Nolan' },
     released: 2008,
+    genre: { name: 'action' },
   },
   {
     title: 'The Godfather',
-    director: 'Francis Ford Coppola',
+    director: { name: 'Francis Ford Coppola' },
     released: 1972,
+    genre: { name: 'crime' },
   },
   {
     title: 'The Shawshenk Redemption',
-    director: 'Frank Darabont',
+    director: { name: 'Frank Darabont' },
     released: 1994,
+    genre: { name: 'thriller' },
   },
   {
     title: 'The Lord of the Rings: The Return of the King',
-    director: 'Peter Jackson',
+    director: { name: 'Peter Jackson' },
     released: 2003,
+    genre: { name: 'fantasy' },
   },
   {
     title: 'Goodfellas',
-    director: 'Martin Scorsese',
+    director: { name: 'Martin Scorsese' },
     released: 1990,
+    genre: { name: 'crime' },
   },
   {
     title: 'The Matrix',
-    director: 'Wachowski sisters',
+    director: { name: 'Wachowski sisters' },
     released: 1999,
+    genre: { name: 'sci-fi' },
   },
   {
     title: 'The Green Mile',
-    director: 'Frank Darabont',
+    director: { name: 'Frank Darabont' },
     released: 1999,
+    genre: { name: 'crime' },
   },
   {
     title: 'Léon: The Professional',
-    director: 'Luc Besson',
+    director: { name: 'Luc Besson' },
     released: 1994,
+    genre: { name: 'action' },
   },
   {
     title: 'the Skipper',
-    director: 'Jacob Goldwasser',
+    director: { name: 'Jacob Goldwasser' },
     released: 1987,
+    genre: { name: 'drama' },
+  },
+];
+
+let users = [
+  {
+    name: 'John Doe',
+    email: 'johndoe@gmail.com',
+    id: 2,
+    favoriteMovies: [],
+  },
+  {
+    name: 'Jean Doe',
+    email: 'jeanndoe@gmail.com',
+    id: 1,
+    favoriteMovies: [],
   },
 ];
 
@@ -63,41 +85,23 @@ app.get('/', (req, res) => {
   res.json({ msg: 'Welcome to my movie app!' });
 });
 
-app.get('/movies', (req, res) => {
-  res.json(myTopTenMovies);
-});
+/* ================= USERS ================= */
 
-app.get('/movies/:title', (req, res) => {
-  const movieTitle = req.params.title;
-  const foundMovie = myTopTenMovies.find((m) => m.title === movieTitle);
-
-  if (!foundMovie) {
-    return res.status(404).json({ msg: `Movie ${movieTitle} is not found` });
-  }
-  res.json(foundMovie);
-});
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Oh no! There is a problem');
-});
-//-----------------------task 2.5-----------
-let users;
-let movies;
-
-//create new user
+// create user
 app.post('/users', (req, res) => {
   const newUser = req.body;
 
   if (newUser.name) {
     newUser.id = uuidv4();
+    newUser.favoriteMovies = [];
     users.push(newUser);
     res.status(201).json(newUser);
   } else {
     res.status(400).send('users need name');
   }
 });
-//update user name
+
+// update user
 app.put('/users/:id', (req, res) => {
   const { id } = req.params;
   const updatedUser = req.body;
@@ -111,90 +115,104 @@ app.put('/users/:id', (req, res) => {
     res.status(400).send('no such user');
   }
 });
-//create favorite movie
+
+// add favorite movie
 app.post('/users/:id/:movieTitle', (req, res) => {
   const { id, movieTitle } = req.params;
-  const updatedUser = req.body;
-
   let user = users.find((user) => user.id == id);
 
   if (user) {
     user.favoriteMovies.push(movieTitle);
-    res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
+    res.status(200).send(`${movieTitle} added to user ${id}`);
   } else {
     res.status(400).send('no such user');
   }
 });
-//delete favorite movie
+
+// delete favorite movie
 app.delete('/users/:id/:movieTitle', (req, res) => {
   const { id, movieTitle } = req.params;
-
   let user = users.find((user) => user.id == id);
 
   if (user) {
     user.favoriteMovies = user.favoriteMovies.filter(
       (title) => title !== movieTitle
     );
-    res
-      .status(200)
-      .send(`${movieTitle} has been removed from user ${id}'s array`);
+    res.status(200).send(`${movieTitle} removed from user ${id}`);
   } else {
     res.status(400).send('no such user');
   }
 });
-//delete user
+
+// delete user
 app.delete('/users/:id', (req, res) => {
   const { id } = req.params;
-
   let user = users.find((user) => user.id == id);
 
   if (user) {
     users = users.filter((user) => user.id != id);
-    res.status(200).send(`user ${id} has been deleted`);
+    res.status(200).send(`user ${id} deleted`);
   } else {
     res.status(400).send('no such user');
   }
 });
-//read
+
+/* ================= MOVIES ================= */
+
+// get all movies
 app.get('/movies', (req, res) => {
   res.status(200).json(movies);
 });
-//read movie title
+
+// get genre
+app.get('/movies/genre/:genreName', (req, res) => {
+  const { genreName } = req.params;
+
+  const movie = movies.find(
+    (m) => m.genre.name.toLowerCase() === genreName.toLowerCase()
+  );
+
+  if (!movie) {
+    return res.status(404).send('no such genre');
+  }
+
+  res.status(200).json(movie.genre);
+});
+
+// get director
+app.get('/movies/director/:directorName', (req, res) => {
+  const { directorName } = req.params;
+
+  const movie = movies.find(
+    (m) => m.director.name.toLowerCase() === directorName.toLowerCase()
+  );
+
+  if (!movie) {
+    return res.status(404).send('no such director');
+  }
+
+  res.status(200).json(movie.director);
+});
+
 app.get('/movies/:title', (req, res) => {
   const { title } = req.params;
-  const movies = movies.find((movie) => movie.Title === title);
+
+  const movie = movies.find(
+    (m) => m.title.toLowerCase() === title.toLowerCase()
+  );
 
   if (movie) {
     res.status(200).json(movie);
   } else {
-    res.status(400).send('no such movie');
+    res.status(404).send('no such movie');
   }
 });
-//read genre
 
-app.get('/movies/genre/:genreName', (req, res) => {
-  const { genreName } = req.params;
-  const genre = movies.find((movie) => movie.Genre.Name === genreName).Genre;
+/* ================= ERROR HANDLER ================= */
 
-  if (genre) {
-    res.status(200).json(genre);
-  } else {
-    res.status(400).send('no such movie');
-  }
-});
-//read direcor
-
-app.get('/movies/director/:directorName', (req, res) => {
-  const { directorName } = req.params;
-  const director = movies.find(
-    (movie) => movie.Director.Name === directorName
-  ).Director;
-
-  if (director) {
-    res.status(200).json(director);
-  } else {
-    res.status(400).send('no such director');
-  }
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Oh no! There is a problem');
 });
 
 app.listen(8080, () => {
